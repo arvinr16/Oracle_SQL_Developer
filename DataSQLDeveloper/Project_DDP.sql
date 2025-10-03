d19233114047bcebd10d6d59ecdf9c4e7c8b43a7
arvinr16 <arviansyahyk@gmail.com>
2025-10-03 09:55:10 +0700

    Adding 'Project_DDP.sql'

---
+++ DataSQLDeveloper/Project_DDP.sql
@@ -0,0 +1,17 @@
+select table_name from user_tables;
+
+-- Table for Pelanggan --
+create table pelanggan (
+    id_pelanggan number,
+    nama varchar2(100),
+    alamat varchar2(200),
+    no_hp varchar2(15),
+    no_ktp integer,
+    email varchar2(50)
+);
+
+create table pegawai (
+    id_pegawai number,
+    
+
+select * from pelanggan;
